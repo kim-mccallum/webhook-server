637fdf6f20ca3270051fb43c82c0250814e79887
Kim McCallum <kchaase@gmail.com>
2021-01-14 12:10:16 -0700

    fixed .env

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send(`Hello world! Welcome to a simple webhook server.`);
+  res.status(200).send(`Hello world! Welcome to a simple webhook server!`);
 });
 
 //the webhook route
