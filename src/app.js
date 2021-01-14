4c36d4c4cf4f2551984e9d991923b101e151db9a
Kim McCallum <kchaase@gmail.com>
2021-01-14 13:20:37 -0700

    enabled CORS to talk to Survey123

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // const HttpError = require("./models/http-error"); ////Use this for simple validation below
 
@@ -12,15 +13,21 @@ const app = express();
 //set up additional middleware and basic routes
 app.use(bodyParser.json());
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.status(200).send(`Hello world! Welcome to a simple webhook server!`);
+  res
+    .status(200)
+    .json({ message: `Hello world! Welcome to a simple webhook server!` });
 });
 
 //the webhook route
 app.post("/catch-data", (req, res, next) => {
   //Later - check the body and make sure that it's from the right place. Validate and handle errors
-  console.log(req.body); //Call action here
-  res.status(200).end(); //respond but add error handling later
+  //   console.log(req.body); //Call action here
+  const email = req.body.feature.attributes.what_is_your_email;
+  console.log(email);
+  res.status(200).json({ message: "received" }).end(); //respond but add error handling later
 });
 
 module.exports = app;
